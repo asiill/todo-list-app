4c8b306dc90944436e6cfa4b189f60063496e06f
asil <9745019+asiill@users.noreply.github.com>
2023-08-18 15:56:05 -0400

    Create Project class

---
+++ src/Project.js
@@ -0,0 +1,44 @@
+/*
+    Project class
+    Manage tasks for a given project
+*/
+
+export default class Project {
+
+    constructor(name) {
+        this.name = name;
+        this.tasks = [];
+    }
+
+    getName() {
+        return this.name;
+    }
+    setName(name) {
+        this.name = name;
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+    setTasks(tasks) {
+        this.tasks = tasks;
+    }
+
+    getTask(taskTitle) {
+        return this.tasks.find(task => task.getTitle() === taskTitle);
+    }
+    isInTasks(taskTitle) {
+        return this.tasks.some(task => task.getTitle() === taskTitle);
+    }
+    addTask(task) {
+        if (this.isInTasks(task.getTitle())){
+            return;
+        }
+        this.tasks.push(task);
+    }
+    deleteTask(task) {
+        let taskIndex = this.tasks.indexOf(task);
+        this.tasks.splice(taskIndex, 1);
+    }
+
+}

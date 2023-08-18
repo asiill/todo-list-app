07f84d5cd402577d96b4560b08a04e1cd27f84d4
asil <9745019+asiill@users.noreply.github.com>
2023-08-18 15:56:19 -0400

    Create List class

---
+++ src/List.js
@@ -0,0 +1,36 @@
+/*
+    List class
+    Manage all projects within the todo list
+*/
+
+export default class List {
+
+    constructor() {
+        this.projects = [];
+    }
+
+    getProjects() {
+        return this.projects;
+    }
+    setProjects(projects) {
+        this.projects = projects;
+    }
+
+    getProject(projectName) {
+        return this.projects.find(project => project.getName() === projectName);
+    }
+    isInProjects(projectName) {
+        return this.projects.some(project => project.getName() === projectName);
+    }
+    addProject(project) {
+        if (this.isInProjects(project.getName())) {
+            return;
+        }
+        this.projects.push(project);
+    }
+    deleteProject(project) {
+        let projectIndex = this.projects.indexOf(project);
+        this.projects.splice(projectIndex, 1);
+    }
+
+}
